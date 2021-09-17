0e92d498aee2a0e8a5c9731884c6d9af731ea0f7
LucasDev13 <lucaspontes140@gmail.com>
2021-09-17 10:27:33 -0300

    Devolvendo elementos com distinct.

--- src/br/com/alura/testeMutableCollections.kt
+++ src/br/com/alura/testeMutableCollections.kt
@@ -1,47 +1,16 @@
 package br.com.alura
 
 fun main() {
-    val bancoDeDados = BancoDeDados()
-    //feito uma cópia/atribuição para a varável nomesSalvos. Essa cópia é por referência.
-    //assim podemos burlar o que foi definido na classe de banco de dados.
-    //podemos mudar o tipo da variável nomesSalvos, mas vamos ter um problema de compilação, mas podemos burlar com
-    //um cast para MutableColletion e funciona normalmente, mas deveria ter um problema, dado que não estamos trabalhando
-    //com Mutable.
-    //Isso acontece porque realmente internamente a instância ainda é de um Mutable, e dado isso essa converção é sim
-    //compatível.
-//    val nomesSalvos: MutableCollection<String> = bancoDeDados.nomes as MutableCollection<String>
-//    nomesSalvos.add("Paulo")// conseguimos adicionar o nome normalmente, mesmo ela estando privada
-    val nomesSalvos: Collection<String> = bancoDeDados.nomes
-    bancoDeDados.salva("Alex")
-    println(nomesSalvos)
-    println(BancoDeDados().nomes)
+    val assistiramCursoAndroid = listOf("Alex", "Fran", "Gui", "Maria")
+    val assistiramCursoKotlin = listOf("Alex", "Paulo", "Maria")
+//    val assistiramAmbos = mutableListOf<String>()
+    val assistiramAmbos = assistiramCursoAndroid + assistiramCursoKotlin
+//    assistiramAmbos.addAll(assistiramCursoAndroid)
+//    assistiramAmbos.addAll(assistiramCursoKotlin)
+    //vai mostrar os elementos repitidos.
+    println("assistiramAmbos repetido> $assistiramAmbos")
+
+    //mostra somente os não repitidos.
+    println("Assistiram ambos distintos ${assistiramAmbos.distinct()}")
 }
 
-class BancoDeDados {
-    //o toList() faz uma snapshot(cópia) real da coleção. Estamos devolvendo uma cópia e dados que estamos devolvendo
-    //uma cópia, naõ vai ser possível modificar os dados reais.
-    val nomes: Collection<String> get() = dados.toList()
-    fun salva(nome: String) {
-        dados.add(nome)
-    }
-
-    companion object{
-        private val dados = mutableListOf<String>()
-    }
-}
-
-fun testaColecao() {
-    val nomes: Collection<String> = mutableListOf(
-        "Alex",
-        "Fran",
-        "Gui",
-        "Maria",
-        "Ana"
-    )
-    for (nome in nomes) {
-        println(nomes)
-    }
-    println(nomes)
-    println("Tem o nome Alex? ${nomes.contains("Alex")}")
-    println("Tamanho da coleção ${nomes.size}")
-}
